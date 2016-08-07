e48335ef1d0cd37249e372890c9cf14f392b5bb9
James Pyne <james.pyne.13@ucl.ac.uk>
2016-08-07 18:36:02 +0100

    Game translated into ES6 OOP style.

--- flappy-bird/main.js
+++ flappy-bird/main.js
@@ -1,37 +1,49 @@
-var game = new Phaser.Game(400, 490, Phaser.AUTO, "gameDiv");
 
-var mainState = {
 
-    preload: function() {
-        if(!game.device.desktop) {
-            game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            game.scale.setMinMax(game.width/2, game.height/2, game.width, game.height);
+
+/*
+NOTE:
+Phaser's 'State' objects each contain a member variable "game" that acts as
+a reference to the current game.
+ */
+
+class Main extends Phaser.State {
+
+    constructor(){
+        super();
+    }
+
+    preload() {
+
+        if(!this.game.device.desktop) {
+            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.game.scale.setMinMax(this.game.width/2, this.game.height/2, this.game.width, this.game.height);
         }
 
-        game.scale.pageAlignHorizontally = true;
-        game.scale.pageAlignVertically = true;
+        this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.pageAlignVertically = true;
 
-        game.stage.backgroundColor = '#71c5cf';
+        this.game.stage.backgroundColor = '#71c5cf';
 
-        game.load.image('pipe', 'assets/pipe.png');
+        this.game.load.image('pipe', 'assets/pipe.png');
 
         // Load the jump sound
-        game.load.audio('jump', 'assets/jump.wav');
+        this.game.load.audio('jump', 'assets/jump.wav');
 
         // Load in spritesheet.
-        game.load.spritesheet('plane', 'assets/plane_scaled.png', 50, 43);
-    },
+        this.game.load.spritesheet('plane', 'assets/plane_scaled.png', 50, 43);
+    }
 
-    create: function() {
-        game.physics.startSystem(Phaser.Physics.ARCADE);
+    create() {
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        this.pipes = game.add.group();
-        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        this.pipes = this.game.add.group();
+        this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);
 
-        this.plane = game.add.sprite(100, 245, 'plane');
+        this.plane = this.game.add.sprite(100, 245, 'plane');
         // Set default frame.
         this.plane.frame = 0;
-        game.physics.arcade.enable(this.plane);
+        this.game.physics.arcade.enable(this.plane);
         this.plane.body.gravity.y = 1000;
 
         // Animate.
@@ -41,30 +53,30 @@ var mainState = {
         // New anchor position
         this.plane.anchor.setTo(-0.2, 0.5);
 
-        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
-        game.input.onDown.add(this.jump, this);
+        this.game.input.onDown.add(this.jump, this);
 
         this.score = 0;
-        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+        this.labelScore = this.game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
 
         // Add the jump sound
-        this.jumpSound = game.add.audio('jump');
+        this.jumpSound = this.game.add.audio('jump');
         this.jumpSound.volume = 0.2;
-    },
+    }
 
-    update: function() {
-        if (this.plane.y < 0 || this.plane.y > game.world.height)
+    update() {
+        if (this.plane.y < 0 || this.plane.y > this.game.world.height)
             this.restartGame();
 
-        game.physics.arcade.overlap(this.plane, this.pipes, this.hitPipe, null, this);
+        this.game.physics.arcade.overlap(this.plane, this.pipes, this.hitPipe, null, this);
 
         // Slowly rotate the plane downward, up to a certain point.
         if (this.plane.angle < 20)
             this.plane.angle += 1;
-    },
+    }
 
-    jump: function() {
+    jump() {
         // If the plane is dead, he can't jump
         if (this.plane.alive == false)
             return;
@@ -72,13 +84,13 @@ var mainState = {
         this.plane.body.velocity.y = -350;
 
         // Jump animation
-        game.add.tween(this.plane).to({angle: -20}, 100).start();
+        this.game.add.tween(this.plane).to({angle: -20}, 100).start();
 
         // Play sound
         this.jumpSound.play();
-    },
+    }
 
-    hitPipe: function() {
+    hitPipe() {
         // If the plane has already hit a pipe, we have nothing to do
         if (this.plane.alive == false)
             return;
@@ -87,29 +99,29 @@ var mainState = {
         this.plane.alive = false;
 
         // Prevent new pipes from appearing
-        game.time.events.remove(this.timer);
+        this.game.time.events.remove(this.timer);
 
         // Go through all the pipes, and stop their movement
         this.pipes.forEach(function(p){
             p.body.velocity.x = 0;
         }, this);
-    },
+    }
 
-    restartGame: function() {
-        game.state.start('main');
-    },
+    restartGame() {
+        this.game.state.start('Main');
+    }
 
-    addOnePipe: function(x, y) {
-        var pipe = game.add.sprite(x, y, 'pipe');
+    addOnePipe(x, y) {
+        var pipe = this.game.add.sprite(x, y, 'pipe');
         this.pipes.add(pipe);
-        game.physics.arcade.enable(pipe);
+        this.game.physics.arcade.enable(pipe);
 
         pipe.body.velocity.x = -200;
         pipe.checkWorldBounds = true;
         pipe.outOfBoundsKill = true;
-    },
+    }
 
-    addRowOfPipes: function() {
+    addRowOfPipes() {
         var hole = Math.floor(Math.random()*5)+1;
 
         for (var i = 0; i < 8; i++)
@@ -118,8 +130,23 @@ var mainState = {
 
         this.score += 1;
         this.labelScore.text = this.score;
-    },
-};
+    }
+}
 
-game.state.add('main', mainState);
-game.state.start('main'); 
+class Game extends Phaser.Game {
+
+    constructor(){
+        super(400, 490, Phaser.AUTO, "gameDiv");
+
+        this.Main = new Main();
+
+        this.state.add('Main', this.Main);
+
+        this.state.start('Main');
+    }
+}
+
+// Run the game!
+// Can instantiate without a reference because we're in the global
+// scope and hence it won't be destroyed.
+new Game();
